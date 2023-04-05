ada7321d73dcc0ce4e45724ac32fbd22c9551a3a
berg8201 <52533671+berg8201@users.noreply.github.com>
2023-04-05 18:59:38 +0200

    Testing

--- java-worker/src/main/java/io/berndruecker/experiments/cloudclient/java/ParallelJobsJavaApplication.java
+++ java-worker/src/main/java/io/berndruecker/experiments/cloudclient/java/ParallelJobsJavaApplication.java
@@ -2,14 +2,11 @@ package io.berndruecker.experiments.cloudclient.java;
 
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.spring.client.EnableZeebeClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableZeebeClient
